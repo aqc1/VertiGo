98b32ca39d21d75f972d0d6f7916f85085528366
aqc1 <aqc1@pct.edu>
2023-01-24 14:10:18 -0500

    chore(project): Renamed due to name collision

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	data, err := os.ReadFile("tests/wordlist.txt")
+	// Temporary test file
+	// This will eventually be a CLI arg
+	handle, err := os.Open("tests/wordlist.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(string(data))
+	defer handle.Close()
+
+	// Grab words from wordlist and add to a slice
+	words := []string{}
+	scanner := bufio.NewScanner(handle)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	// Process slice here
+	appended := addAfter.appendCharacter(words)
+	mangled := append(words, appended)
+
+	for _, word := range mangled {
+		fmt.Println(word)
+	}
 }
